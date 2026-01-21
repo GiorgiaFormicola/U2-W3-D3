f8c61095e90569c4d25bede79ebb185431ab31ee
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-01-21 17:08:25 +0100

    Project done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // save the library ENDPOINT
 const libraryURL = "https://striveschool-api.herokuapp.com/books";
 
-//create a function that create a card
+//create a function that create a card with discard button
 const createBookCard = function (book) {
   const bookTitle = book.title;
   const bookImage = book.img;
@@ -15,11 +15,22 @@ const createBookCard = function (book) {
               <div class="card-body text-center d-md-flex flex-column gap-md-3 justify-content-end">
                 <h5 class="card-title fs-2">${bookTitle}</h5>
                 <p class="card-text fs-4">&euro; <span class="fs-4">${bookPrice}</span></p>
-                <a href="" class="btn btn-outline-light fs-4 mb-1" id="discardButton">Discard</a>
+                <button href="" class="btn btn-outline-light fs-4 mb-1 discard-button">Discard</a>
               </div>
             </div>
           </div>
   `;
+
+  const allDiscardButtons = document.querySelectorAll(".discard-button");
+  const allCards = document.querySelectorAll(".col");
+
+  allDiscardButtons.forEach((button, i) => {
+    const card = allCards[i];
+    button.addEventListener("click", function (e) {
+      e.preventDefault;
+      card.classList.add("d-none");
+    });
+  });
 };
 
 // create a function that make an HTTP REQUEST
